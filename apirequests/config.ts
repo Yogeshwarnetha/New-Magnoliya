10423d3eddab8acb1ddddc4c38a65d1786e9a4fb
Yogeshwarnetha <gadde.yogeshwar@gmail.com>
2025-10-23 17:21:17 +0530

    Added

--- apirequests/config.ts
+++ apirequests/config.ts
@@ -1,5 +1,5 @@
 const env: any = "Production"; // Change to "dev" for development environment
 
-export const origin = env == "dev" ? "http://localhost:4002" : "https://api.1728hosting.cloud/"
+export const origin = env == "dev" ? "http://localhost:4002" : "https://api.1728hosting.cloud"
 
 export const adminCookie = "faf61ac9d4fdb0000025bdf7375e4fc9020000"
